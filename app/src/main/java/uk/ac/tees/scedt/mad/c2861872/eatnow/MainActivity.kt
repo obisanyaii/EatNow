6ee7bd648c12414bdad28f16578415446cf7830c
c2861872 <c2861872@live.tees.ac.uk>
2024-03-01 16:41:42 +0000

    Initial commit

---
+++ app/src/main/java/uk/ac/tees/scedt/mad/c2861872/eatnow/MainActivity.kt
@@ -0,0 +1,46 @@
+package uk.ac.tees.scedt.mad.c2861872.eatnow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import uk.ac.tees.scedt.mad.c2861872.eatnow.ui.theme.EatNowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            EatNowTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EatNowTheme {
+        Greeting("Android")
+    }
+}
